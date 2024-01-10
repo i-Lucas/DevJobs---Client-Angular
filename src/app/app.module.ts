b358e9211f41a2ff742c99662d9a95dfe5eae70a
i-Lucas <novo.contato.lucas@gmail.com>
2024-01-09 21:40:37 -0300

    feature: updating app module providers to intercept global requests and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppService } from '@app-services/app/app.service';
+import { HttpService } from '@app-services/http/http.service';
+import { AuthenticationService } from '@app-services/auth/auth.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +16,20 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    AppService,
+    HttpService,
+    AuthenticationService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpService,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

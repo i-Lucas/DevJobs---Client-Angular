4fbbd2d865ce333f25572691c8c25a75219c1cb2
i-Lucas <novo.contato.lucas@gmail.com>
2024-01-09 21:50:20 -0300

    feature: root signup component mode selection

--- src/app/pages/authentication/features/signup/container/signup-root.component.ts
+++ src/app/pages/authentication/features/signup/container/signup-root.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class SignupRootComponent {
 
+  private signupUrl: string = '/auth/signup/developer';
+  protected mode: 'DEV' | 'COMPANY' = location.pathname === this.signupUrl ? 'DEV' : 'COMPANY';
+
+  protected changeMode(selectedMode: 'DEV' | 'COMPANY'): void {
+    this.mode = selectedMode;
+  }
 }

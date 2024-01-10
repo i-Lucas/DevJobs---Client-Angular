4cdcf889fb0c18538c5795b9fcee45d1ca135830
i-Lucas <novo.contato.lucas@gmail.com>
2024-01-10 17:03:46 -0300

    refactor: use the base class method for simplification

--- src/app/pages/authentication/components/auth-header/auth-header.component.ts
+++ src/app/pages/authentication/components/auth-header/auth-header.component.ts
@@ -1,36 +1,21 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Subject, takeUntil } from 'rxjs';
+import { Component } from '@angular/core';
+
+import { MessageService } from 'primeng/api';
 
 import { AppService } from '@app-services/app/app.service';
+import { BaseComponentService } from '@app-services/components/base-component.service';
 
 @Component({
   selector: 'app-auth-header',
   templateUrl: './auth-header.component.html',
 })
-export class AuthHeaderComponent implements OnDestroy {
+export class AuthHeaderComponent extends BaseComponentService {
 
-  private destroy$ = new Subject<void>();
-  protected loading: boolean = false;
-
-  constructor(private appService: AppService) {
-    this.subscribeToAppState();
-  }
-
-  ngOnDestroy() {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
-
-  private subscribeToAppState() {
-    this.appService.getIsRequestInProgress()
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((state) => {
-        this.loading = state;
-      });
-  }
-
-  protected isRoute(path: string): any {
-    return location.pathname.includes(path)
+  constructor(
+    protected override messageService: MessageService,
+    protected override appService: AppService,
+  ) {
+    super(appService, messageService);
   }
 
 }

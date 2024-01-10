578e91173183aeb804d19f04d67d170bc185644a
i-Lucas <novo.contato.lucas@gmail.com>
2024-01-10 11:14:50 -0300

    feature: transfer shared functionality of adding listener to form field

--- src/app/shared/forms/services/base-form.service.ts
+++ src/app/shared/forms/services/base-form.service.ts
@@ -1,12 +1,50 @@
-import { AbstractControl, ValidatorFn } from '@angular/forms';
+import { Injectable, OnDestroy } from '@angular/core';
 
-export class BaseFormService {
+interface SetFormControlListenerInput {
+  formControl: AbstractControl | null,
+  callbackFunction: (formControl: AbstractControl) => void
+}
+
+import {
+  AbstractControl,
+  ValidatorFn
+} from '@angular/forms';
+
+import {
+  Subject,
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  takeUntil
+} from 'rxjs';
+
+@Injectable()
+export class BaseFormService implements OnDestroy {
+
+  protected destroy$ = new Subject<void>();
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   protected validatePattern(pattern: RegExp): ValidatorFn {
     return (control: AbstractControl): { [key: string]: any } | null => {
       const isValid = pattern.test(control.value);
       return isValid ? null : { customPattern: true };
-    };
-  };
+    }
+  }
+
+  protected setFormControlListener({ formControl, callbackFunction }: SetFormControlListenerInput) {
+
+    formControl?.valueChanges
+      .pipe(
+        debounceTime(800),
+        distinctUntilChanged(),
+        filter(() => formControl.valid),
+        takeUntil(this.destroy$)
+      )
+      .subscribe(() => callbackFunction(formControl));
+  }
 
 }

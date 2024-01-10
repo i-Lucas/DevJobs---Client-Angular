09628c758070fca5fa1ff6705f1ae301a6c1bf6a
i-Lucas <novo.contato.lucas@gmail.com>
2024-01-09 21:36:51 -0300

    feature: service to store and manage some global states

---
+++ src/app/shared/services/app/app.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  private isHttpRequestInProgress = new BehaviorSubject<boolean>(false);
+
+  private GlobalAppSidebarState = new BehaviorSubject<boolean>(false);
+
+  public getSidebarState(): Observable<boolean> {
+    return this.GlobalAppSidebarState.asObservable();
+  }
+
+  public toggleSidebar(): void {
+    const currentSidebarState = this.GlobalAppSidebarState.getValue();
+    this.GlobalAppSidebarState.next(!currentSidebarState);
+  }
+
+  public getIsRequestInProgress(): Observable<boolean> {
+    return this.isHttpRequestInProgress.asObservable();
+  }
+  
+  public updateRequestInProgress(state: boolean): void {
+    this.isHttpRequestInProgress.next(state);
+  }
+
+}

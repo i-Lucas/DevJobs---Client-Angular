ab0556de1befb4a3a57e75329de6bac27edab523
i-Lucas <novo.contato.lucas@gmail.com>
2024-01-09 21:52:18 -0300

    update: update overview page header

--- src/app/pages/overview/components/overview-header/overview-header.component.ts
+++ src/app/pages/overview/components/overview-header/overview-header.component.ts
@@ -3,8 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-overview-header',
   templateUrl: './overview-header.component.html',
-  styleUrl: './overview-header.component.scss'
 })
-export class OverviewHeaderComponent {
-
-}
+export class OverviewHeaderComponent {}

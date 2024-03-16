e08ff2a11692f4bd2c8da5c342c8715f3a239e0e
i-Lucas <novo.contato.lucas@gmail.com>
2024-03-16 17:39:08 -0300

    partial: feat chart.js

--- src/app/pages/dashboard/features/company/home/components/root-home/root-home.component.ts
+++ src/app/pages/dashboard/features/company/home/components/root-home/root-home.component.ts
@@ -1,9 +1,386 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { ChartOptions } from 'chart.js';
+
+import { AppStateService } from '@app-services/app/app.service';
 
 @Component({
   selector: 'app-root-home',
   templateUrl: './root-home.component.html',
 })
-export class RootHomeComponent {
+export class RootHomeComponent implements OnDestroy {
 
+  f_data: any;
+  f_options: any;
+
+  x_data: any;
+  x_options: any;
+
+  a_data: any;
+  a_options: ChartOptions | undefined;
+
+  b_data: any;
+  b_options: any;
+
+  l_data: any
+  l_options: any
+
+  t_data: any
+  t_options: any
+
+  g_data: any
+  g_options: ChartOptions | undefined;
+
+  protected loading: boolean = false;
+  protected destroy$ = new Subject<void>();
+
+  constructor(private appService: AppStateService) {
+
+    this.appService.getIsRequestInProgress()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({ next: (state) => this.loading = state })
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+
+  ngOnInit() {
+
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--text-color');
+
+    const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+    const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+
+    // -----------------------------------------------------------------------------------------------------------
+
+    this.a_data = {
+      labels: ['A', 'B', 'C'],
+      datasets: [
+        {
+          data: [540, 325, 702],
+          backgroundColor: [documentStyle.getPropertyValue('--blue-500'), documentStyle.getPropertyValue('--yellow-500'), documentStyle.getPropertyValue('--green-500')],
+          hoverBackgroundColor: [documentStyle.getPropertyValue('--blue-400'), documentStyle.getPropertyValue('--yellow-400'), documentStyle.getPropertyValue('--green-400')]
+        }
+      ]
+    };
+
+    this.a_options = {
+      plugins: {
+        legend: {
+          labels: {
+            usePointStyle: true,
+            color: textColor
+          }
+        }
+      }
+    };
+
+    // -----------------------------------------------------------------------------------------------------------
+
+    this.x_data = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'Dataset 1',
+          fill: false,
+          borderColor: documentStyle.getPropertyValue('--blue-500'),
+          yAxisID: 'y',
+          tension: 0.4,
+          data: [65, 59, 80, 81, 56, 55, 10]
+        },
+        {
+          label: 'Dataset 2',
+          fill: false,
+          borderColor: documentStyle.getPropertyValue('--green-500'),
+          yAxisID: 'y1',
+          tension: 0.4,
+          data: [28, 48, 40, 19, 86, 27, 90]
+        }
+      ]
+    };
+
+    this.x_options = {
+      stacked: false,
+      maintainAspectRatio: false,
+      aspectRatio: 0.6,
+      plugins: {
+        legend: {
+          labels: {
+            color: textColor
+          }
+        }
+      },
+      scales: {
+        x: {
+          ticks: {
+            color: textColorSecondary
+          },
+          grid: {
+            color: surfaceBorder
+          }
+        },
+        y: {
+          type: 'linear',
+          display: true,
+          position: 'left',
+          ticks: {
+            color: textColorSecondary
+          },
+          grid: {
+            color: surfaceBorder
+          }
+        },
+        y1: {
+          type: 'linear',
+          display: true,
+          position: 'right',
+          ticks: {
+            color: textColorSecondary
+          },
+          grid: {
+            drawOnChartArea: false,
+            color: surfaceBorder
+          }
+        }
+      }
+    }
+
+    // -----------------------------------------------------------------------------------------------------------
+
+
+    this.f_data = {
+      labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+      datasets: [
+        {
+          label: 'Sales',
+          data: [540, 325, 702, 620],
+          backgroundColor: ['rgba(255, 159, 64, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(153, 102, 255, 0.2)'],
+          borderColor: ['rgb(255, 159, 64)', 'rgb(75, 192, 192)', 'rgb(54, 162, 235)', 'rgb(153, 102, 255)'],
+          borderWidth: 1
+        }
+      ]
+    };
+
+    this.f_options = {
+      plugins: {
+        legend: {
+          labels: {
+            color: textColor
+          }
+        }
+      },
+      scales: {
+        y: {
+          beginAtZero: true,
+          ticks: {
+            color: textColorSecondary
+          },
+          grid: {
+            color: surfaceBorder,
+            drawBorder: false
+          }
+        },
+        x: {
+          ticks: {
+            color: textColorSecondary
+          },
+          grid: {
+            color: surfaceBorder,
+            drawBorder: false
+          }
+        }
+      }
+    };
+
+    // -----------------------------------------------------------------------------------------------------------
+
+    this.b_data = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'My First dataset',
+          backgroundColor: documentStyle.getPropertyValue('--blue-500'),
+          borderColor: documentStyle.getPropertyValue('--blue-500'),
+          data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+          label: 'My Second dataset',
+          backgroundColor: documentStyle.getPropertyValue('--pink-500'),
+          borderColor: documentStyle.getPropertyValue('--pink-500'),
+          data: [28, 48, 40, 19, 86, 27, 90]
+        }
+      ]
+    }
+
+    this.b_options = {
+      indexAxis: 'y',
+      maintainAspectRatio: false,
+      aspectRatio: 0.8,
+      plugins: {
+        legend: {
+          labels: {
+            color: textColor
+          }
+        }
+      },
+      scales: {
+        x: {
+          ticks: {
+            color: textColorSecondary,
+            font: {
+              weight: 500
+            }
+          },
+          grid: {
+            color: surfaceBorder,
+            drawBorder: false
+          }
+        },
+        y: {
+          ticks: {
+            color: textColorSecondary
+          },
+          grid: {
+            color: surfaceBorder,
+            drawBorder: false
+          }
+        }
+      }
+    };
+
+    // -----------------------------------------------------------------------------------------------------------
+
+    this.l_data = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          type: 'bar',
+          label: 'Dataset 1',
+          backgroundColor: documentStyle.getPropertyValue('--blue-500'),
+          data: [50, 25, 12, 48, 90, 76, 42]
+        },
+        {
+          type: 'bar',
+          label: 'Dataset 2',
+          backgroundColor: documentStyle.getPropertyValue('--green-500'),
+          data: [21, 84, 24, 75, 37, 65, 34]
+        },
+        {
+          type: 'bar',
+          label: 'Dataset 3',
+          backgroundColor: documentStyle.getPropertyValue('--yellow-500'),
+          data: [41, 52, 24, 74, 23, 21, 32]
+        }
+      ]
+    };
+
+    this.l_options = {
+      maintainAspectRatio: false,
+      aspectRatio: 0.8,
+      plugins: {
+        tooltip: {
+          mode: 'index',
+          intersect: false
+        },
+        legend: {
+          labels: {
+            color: textColor
+          }
+        }
+      },
+      scales: {
+        x: {
+          stacked: true,
+          ticks: {
+            color: textColorSecondary
+          },
+          grid: {
+            color: surfaceBorder,
+            drawBorder: false
+          }
+        },
+        y: {
+          stacked: true,
+          ticks: {
+            color: textColorSecondary
+          },
+          grid: {
+            color: surfaceBorder,
+            drawBorder: false
+          }
+        }
+      }
+    }
+
+
+    // -----------------------------------------------------------------------------------------------------------
+
+
+    this.t_data = {
+      labels: ['A', 'B', 'C'],
+      datasets: [
+        {
+          data: [300, 50, 100],
+          backgroundColor: [documentStyle.getPropertyValue('--blue-500'), documentStyle.getPropertyValue('--yellow-500'), documentStyle.getPropertyValue('--green-500')],
+          hoverBackgroundColor: [documentStyle.getPropertyValue('--blue-400'), documentStyle.getPropertyValue('--yellow-400'), documentStyle.getPropertyValue('--green-400')]
+        }
+      ]
+    };
+
+
+    this.t_options = {
+      cutout: '60%',
+      plugins: {
+        legend: {
+          labels: {
+            color: textColor
+          }
+        }
+      }
+    };
+
+    // -----------------------------------------------------------------------------------------------------------
+
+    this.g_data = {
+      datasets: [
+        {
+          data: [11, 16, 7, 3, 14],
+          backgroundColor: [
+            documentStyle.getPropertyValue('--red-500'),
+            documentStyle.getPropertyValue('--green-500'),
+            documentStyle.getPropertyValue('--yellow-500'),
+            documentStyle.getPropertyValue('--bluegray-500'),
+            documentStyle.getPropertyValue('--blue-500')
+          ],
+          label: 'My dataset'
+        }
+      ],
+      labels: ['Red', 'Green', 'Yellow', 'Grey', 'Blue']
+    };
+
+    this.g_options = {
+      plugins: {
+        legend: {
+          labels: {
+            color: textColor
+          }
+        }
+      },
+      scales: {
+        r: {
+          grid: {
+            color: surfaceBorder
+          }
+        }
+      }
+    };
+
+  }
+
+
+  // -----------------------------------------------------------------------------------------------------------
 }
--- src/app/pages/dashboard/features/company/home/home.module.ts
+++ src/app/pages/dashboard/features/company/home/home.module.ts
@@ -3,6 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { RootHomeComponent } from './components/root-home/root-home.component';
+import { SharedComponentsModule } from 'app/shared/components/shared-components.module';
+
+import { ChartModule } from 'primeng/chart';
+import { TooltipModule } from 'primeng/tooltip';
+import { DividerModule } from 'primeng/divider';
 
 const routes: Routes = [
   {
@@ -12,9 +17,17 @@ const routes: Routes = [
 ]
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RootHomeComponent,
+  ],
   imports: [
     CommonModule,
+
+    ChartModule,
+    TooltipModule,
+    DividerModule,
+
+    SharedComponentsModule,
     RouterModule.forChild(routes)
   ]
 })
